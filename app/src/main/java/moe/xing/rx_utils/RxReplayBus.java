e8ef281e512006a925ff5797cfb9e6564a57b42a
qixingchen <qixingchen@lovelive.moe>
2016-11-23 14:40:47 +0800

    add rx replay bus

--- app/src/main/java/moe/xing/rx_utils/RxReplayBus.java
+++ app/src/main/java/moe/xing/rx_utils/RxReplayBus.java
@@ -1,41 +1,36 @@
 package moe.xing.rx_utils;
 
 import rx.Observable;
-import rx.subjects.PublishSubject;
+import rx.subjects.ReplaySubject;
 import rx.subjects.SerializedSubject;
 import rx.subjects.Subject;
 
 /**
  * Created by Qi Xingchen on 16-11-23.
- * <p>
- * rx bus
  */
 
-public class RxBus {
+public class RxReplayBus {
 
-    private static RxBus instance;
-    private final Subject<Object, Object> _bus = new SerializedSubject<>(PublishSubject.create());
+    private static RxReplayBus instance;
+    private final Subject<Object, Object> _bus = new SerializedSubject<>(ReplaySubject.create());
 
-    public static RxBus getInstance() {
+    public static RxReplayBus getInstance() {
         if (instance == null) {
             synchronized (RxBus.class) {
                 if (instance == null) {
-                    instance = new RxBus();
+                    instance = new RxReplayBus();
                 }
             }
         }
         return instance;
     }
 
-    public static RxBus getNewInstance() {
-        return new RxBus();
-    }
-
     public void send(Object o) {
         _bus.onNext(o);
     }
 
-    public Observable<Object> toObserverable() {
+    public Observable<Object> toObservable() {
         return _bus;
     }
+
 }
