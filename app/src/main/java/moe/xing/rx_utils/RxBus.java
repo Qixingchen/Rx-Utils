d4e2174cf02aedf6d0f69b7fd627139408fb82ef
qixingchen <qixingchen@lovelive.moe>
2016-11-23 10:53:49 +0800

    add rx bus

---
+++ app/src/main/java/moe/xing/rx_utils/RxBus.java
@@ -0,0 +1,29 @@
+package moe.xing.rx_utils;
+
+import rx.Observable;
+import rx.subjects.PublishSubject;
+import rx.subjects.SerializedSubject;
+import rx.subjects.Subject;
+
+/**
+ * Created by Qi Xingchen on 16-11-23.
+ * <p>
+ * rx bus
+ */
+
+public class RxBus {
+
+    private final Subject<Object, Object> _bus = new SerializedSubject<>(PublishSubject.create());
+
+    public static RxBus getNewInstance() {
+        return new RxBus();
+    }
+
+    public void send(Object o) {
+        _bus.onNext(o);
+    }
+
+    public Observable<Object> toObserverable() {
+        return _bus;
+    }
+}
