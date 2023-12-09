c0e2d48ff2d1bc386da2b873cb350a08d5c4d0ea
Qixingchen <i@loli.xing.moe>
2023-12-09 15:34:51 +0800

    split rx and rx2 -2

--- sample/src/main/java/moe/xing/rx_utils_sample/MainActivity.java
+++ sample/src/main/java/moe/xing/rx_utils_sample/MainActivity.java
@@ -6,17 +6,18 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-import com.jakewharton.rxbinding2.view.RxView;
+import com.jakewharton.rxbinding.view.RxView;
 
+import java.io.IOException;
 import java.util.HashMap;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import moe.xing.baseutils.Init;
 import moe.xing.baseutils.utils.FileUtils;
-import moe.xing.rx2_utils.RxFileUtils;
-import moe.xing.rx2_utils.RxViewEvent;
+import moe.xing.rx_utils.RxFileUtils;
+import moe.xing.rx_utils.RxViewEvent;
+import rx.android.schedulers.AndroidSchedulers;
 
 /**
  * Created by Qi Xingchen on 17-6-14.
@@ -35,21 +36,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Init.init(getApplication(), true, "1.0", "rx_util");
 
-        Disposable disposable = RxView.clicks(findViewById(R.id.copy_file_button)).compose(RxViewEvent.delay())
-                .subscribe(o -> {
-                    Disposable dis = RxFileUtils.SaveString("2333", FileUtils.getCacheFile("2333.txt"))
-                            .flatMap(file -> {
-                                HashMap<String, String> replace = new HashMap<>();
-                                replace.put("333", "222");
-                                return RxFileUtils.replaceStringsInfile(file, replace);
-
-                            })
-                            .observeOn(AndroidSchedulers.mainThread())
-                            .subscribe(file -> Toast.makeText(MainActivity.this, file.getAbsolutePath() + " saved", Toast.LENGTH_LONG).show());
-
-                    compositeDisposable.add(dis);
-                });
-        compositeDisposable.add(disposable);
+//        Disposable disposable = RxView.clicks(findViewById(R.id.copy_file_button)).compose(RxViewEvent.delay())
+//                .subscribe(o -> {
+//                    Disposable dis = null;
+//                    try {
+//                        dis = RxFileUtils.SaveString("2333", FileUtils.getCacheFile("2333.txt"))
+//                                .flatMap(file -> {
+//                                    HashMap<String, String> replace = new HashMap<>();
+//                                    replace.put("333", "222");
+//                                    return RxFileUtils.replaceStringsInfile(file, replace);
+//
+//                                })
+//                                .observeOn(AndroidSchedulers.mainThread())
+//                                .subscribe(file -> Toast.makeText(MainActivity.this, file.getAbsolutePath() + " saved", Toast.LENGTH_LONG).show());
+//                    } catch (IOException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//
+//                    compositeDisposable.add(dis);
+//                });
+//        compositeDisposable.add(disposable);
     }
 
     @Override
